4abc4841b1ce5d29563b498e932c792d2c05c8ac
nemocrow <kuzyasoft@yandex.ru>
2021-01-28 11:54:04 +0300

    Add files via upload

---
+++ src/Authority/AuthorityL5/AuthorityL5ServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Authority\AuthorityL5;
+
+use Authority\Authority;
+use Illuminate\Support\ServiceProvider;
+
+class AuthorityL5ServiceProvider extends ServiceProvider {
+    
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+    public function boot() {
+        // Publish config
+        $this->publishes([
+            __DIR__ . '/../../config/config.php' => config_path('authority.php'),
+        ]);
+        
+        // Publish migrations
+        $this->publishes([
+            __DIR__ . '/../../migrations/' => base_path('/database/migrations')
+        ], 'migrations');
+    }
+    
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+        $this->app['authority'] = $this->app->share(function($app){
+            $user = $app['auth']->user();
+            
+            $authority = new Authority($user);
+            
+            $initialize = $app['config']->get('authority.initialize', null);
+
+            if ($initialize) {
+                $initialize($authority);
+            }
+
+            return $authority;
+        });
+        
+        $this->app->alias('authority', 'Authority\Authority');
+    }
+
+}
